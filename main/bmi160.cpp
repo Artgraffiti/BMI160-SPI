dc68a53b74dc5168201d3ced28e6dcaa33a4efc7
Alexandr Vasilev <sanchous.vas@gmail.com>
2024-07-06 05:22:24 +0300

    Update

--- main/bmi160.cpp
+++ main/bmi160.cpp
@@ -11,8 +11,8 @@
 
 // #define __DEBUG__
 
-#define CMD_READ    0x80
-#define CMD_WRITE   0x7F
+#define CMD_READ    0x01
+#define CMD_WRITE   0x00
 
 static const char *TAG = "IMU";
 
@@ -32,37 +32,30 @@ int8_t user_spi_read(uint8_t dev_addr, uint8_t reg_addr, uint8_t *read_data, uin
     memset(&t, 0, sizeof(t));
 
     // Memory allocation with 32-bit alignment for more efficient transactions
-    uint8_t *send_data = (uint8_t*) heap_caps_aligned_alloc(32, 1, MALLOC_CAP_DMA);
-    uint8_t *recv_data = (uint8_t*) heap_caps_aligned_alloc(32, len+1, MALLOC_CAP_DMA);
+    uint8_t *recv_data = (uint8_t*) heap_caps_aligned_alloc(32, len, MALLOC_CAP_DMA);
 
-    if (send_data == NULL || recv_data == NULL) {
+    if (recv_data == NULL) {
         ESP_LOGE(TAG, "Failed to allocate memory for buffers");
-        if (send_data) free(send_data);
-        if (recv_data) free(recv_data);
         return -1;
     }
 
-    // 1 bit command. 7 bit register address
-    send_data[0] = CMD_READ | reg_addr;
-    
     // Setting up transaction
-    t.length = 8 * (len + 1);
-    t.tx_buffer = send_data;
+    t.length = 8 * len;
+    t.cmd = CMD_READ;
+    t.addr = reg_addr;
     t.rx_buffer = recv_data;
 
     ret = spi_device_polling_transmit(spi, &t);
     if (ret != ESP_OK) {
         ESP_LOGE(TAG, "Error when reading SPI data: %s", esp_err_to_name(ret));
-        heap_caps_free(send_data);
         heap_caps_free(recv_data);
         return -1;
     }
 
-    // Copy the received data (excluding the first byte)
-    memcpy(read_data, &recv_data[1], len);
+    // Copy the received data
+    memcpy(read_data, recv_data, len);
 
     // Freeing allocated memory
-    heap_caps_free(send_data);
     heap_caps_free(recv_data);
 
 #ifdef __DEBUG__
@@ -76,22 +69,22 @@ int8_t user_spi_write(uint8_t dev_addr, uint8_t reg_addr, uint8_t *write_data, u
     spi_transaction_t t;
     memset(&t, 0, sizeof(t));
 
-    // Memory allocation with 32-bit alignment for more efficient transactions
-    uint8_t *send_data = (uint8_t*) heap_caps_aligned_alloc(32, len + 1, MALLOC_CAP_DMA);
+    // Memory allocation with 32-bit alignment for more efficient transactions with DMA
+    uint8_t *send_data = (uint8_t*) heap_caps_aligned_alloc(32, len, MALLOC_CAP_DMA);
 
     if (send_data == NULL) {
         ESP_LOGE(TAG, "Failed to allocate memory for the buffer");
         return -1;
     }
 
-    // 1 bit command. 7 bit register address. Rest is data
-    send_data[0] = CMD_WRITE & reg_addr;
-    memcpy(&send_data[1], write_data, len);
+    // Сopy information for sending
+    memcpy(send_data, write_data, len);
 
     // Setting up transaction
-    t.length = (len + 1) * 8;
+    t.length = 8 * len;
+    t.cmd = CMD_WRITE;
+    t.addr = reg_addr;
     t.tx_buffer = send_data;
-    t.flags = SPI_TRANS_DMA_BUFFER_ALIGN_MANUAL;
 
     ret = spi_device_polling_transmit(spi, &t);
     if (ret != ESP_OK) {
--- main/main.cpp
+++ main/main.cpp
@@ -33,6 +33,8 @@ void spi_init() {
     };
     
     spi_device_interface_config_t devcfg = {
+        .command_bits = 1,                      // R/W
+        .address_bits = 7,                      // Register address
         .mode = 0,                              // SPI mode (CPOL, CPHA) -> (0, 0). p.89 bmi160 ds
         .clock_speed_hz = 10 * 1000 * 1000,     // 10 MHz
         .spics_io_num = CONFIG_GPIO_CS,         // CS pin
